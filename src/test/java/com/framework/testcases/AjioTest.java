0ed4e04628c9e5528f8ec7dfc3da2ee0f4a28b3e
Bhavik1191 <bhavikkshah91@gmail.com>
2021-12-21 22:40:44 +0530

    paramterised with testng browsertype

--- src/test/java/com/framework/testcases/AjioTest.java
+++ src/test/java/com/framework/testcases/AjioTest.java
@@ -4,30 +4,36 @@ package com.framework.testcases;
 import com.framework.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class AjioTest {
 
     public static WebDriver driver;
 
+    @Parameters("browserType")
     @BeforeTest
-    public void start()
+    public void start(String browserType)
     {
+
         DriverFactory driverFactory = new DriverFactory();
-        driver = driverFactory.init_browser("chrome");
+        driver = driverFactory.init_browser(browserType);
     }
 
-    @Test
+    @DataProvider(name = "test Data")
+    public Object[][] testData()
+    {
+        return new Object[][]{{"test date 1"},{"test data 2"}};
+    }
+
+    @Test()
     public void SelectMenShirt()
     {
         driver.get("https://www.ajio.com");
+        System.out.println("Print Test data" );
     }
 
     @AfterTest
     public void tearDown() {
-        driver.close();
         driver.quit();
     }
 }
--- src/test/java/com/framework/testcases/RedBusTest.java
+++ src/test/java/com/framework/testcases/RedBusTest.java
@@ -4,17 +4,19 @@ import com.framework.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class RedBusTest {
 
     public static WebDriver driver;
 
+    @Parameters("browserType")
     @BeforeTest
-    public void start()
+    public void start(String browserType)
     {
         DriverFactory driverFactory = new DriverFactory();
-        driver = driverFactory.init_browser("chrome");
+        driver = driverFactory.init_browser(browserType);
     }
 
     @Test
